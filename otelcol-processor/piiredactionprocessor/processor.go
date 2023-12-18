b063cb498f9421e0ad7162b93d292f8d57fbc4f0
Andrzej Stencel <astencel@sumologic.com>
2023-12-18 15:45:51 +0100

    feat(otelcol/piiredactionprocessor): redact whole body
    This doesn't use the AI model yet, but it modifies all log records by replacing the whole log body with a series of asterisks "*".

--- otelcol-processor/piiredactionprocessor/processor.go
+++ otelcol-processor/piiredactionprocessor/processor.go
@@ -2,6 +2,7 @@ package piiredactionprocessor
 
 import (
 	"context"
+	"strings"
 
 	"go.opentelemetry.io/collector/pdata/plog"
 	"go.uber.org/zap"
@@ -18,5 +19,22 @@ func newPiiRedactionProcessor(ctx context.Context, config *Config, logger *zap.L
 }
 
 func (p *piiRedactionProcessor) processLogs(ctx context.Context, batch plog.Logs) (plog.Logs, error) {
+	resourcesLogs := batch.ResourceLogs()
+	for i := 0; i < resourcesLogs.Len(); i++ {
+		resource := resourcesLogs.At(i)
+		scopeLogs := resource.ScopeLogs()
+		for j := 0; j < scopeLogs.Len(); j++ {
+			scope := scopeLogs.At(j)
+			logRecords := scope.LogRecords()
+			for k := 0; k < logRecords.Len(); k++ {
+				record := logRecords.At(k)
+				redactPii(ctx, record)
+			}
+		}
+	}
 	return batch, nil
+}
+
+func redactPii(ctx context.Context, record plog.LogRecord) {
+	record.Body().SetStr(strings.Repeat("*", len(record.Body().AsString())))
 }
